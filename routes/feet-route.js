44e0032c5a59728fb11c92e28c81c40ae099f42d
V <60380753+09q@users.noreply.github.com>
2021-12-07 11:01:47 -0300

    Update feet-route.js

--- routes/feet-route.js
+++ routes/feet-route.js
@@ -1,6 +1,6 @@
 module.exports = function(app, neko) {
 	app.get('/feet', function(req, res) {
-		console.log('Recebido uma conexão na API. (feet)')
+		console.log('Recebido uma conexão na API. [ feet ]')
 
 	async function nekoGirl() {
 		res.send(await neko.nsfw.feet());
