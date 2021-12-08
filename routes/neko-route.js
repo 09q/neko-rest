ce9c4d513105abcd6332cd0bc70eaaacdaa4b2ec
V <60380753+09q@users.noreply.github.com>
2021-12-08 12:17:20 -0300

    Update neko-route.js

--- routes/neko-route.js
+++ routes/neko-route.js
@@ -1,6 +1,6 @@
 module.exports = function(app, neko) {
 	app.get('/neko', function(req, res) {
-		console.log('Recebido uma conexão na API. (neko)')
+		console.log('Recebido uma conexão na API. [ neko ] ')
 
 	async function nekoGirl() {
 		res.send(await neko.nsfw.neko());
