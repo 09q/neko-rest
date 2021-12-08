16753d4433bdc5908266c83d3c01b16ee51411be
V <60380753+09q@users.noreply.github.com>
2021-12-08 12:17:36 -0300

    Update avatar-route.js

--- routes/avatar-route.js
+++ routes/avatar-route.js
@@ -1,6 +1,6 @@
 module.exports = function(app, neko) {
 	app.get('/avatar', function(req, res) {
-		console.log('Recebido uma conexão na API. (avatar)')
+		console.log('Recebido uma conexão na API. [ avatar ]')
 
 	async function nekoGirl() {
 		res.send(await neko.nsfw.avatar());
